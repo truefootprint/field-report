3bee2b882192c05dc42a9714d0753f8ec8d3b48d
Chanda <mulaikopwapwa@gmail.com>
2020-07-30 08:24:55 +0200

    Updates

---
+++ precache-manifest.2a480511f097621dea6d62d3d827f60e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5bfe0852988fe55f505879723844c81",
+    "url": "/field-report/index.html"
+  },
+  {
+    "revision": "c9d13f458001f971e1c2",
+    "url": "/field-report/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3cb18f316a2367062ef3",
+    "url": "/field-report/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c9d13f458001f971e1c2",
+    "url": "/field-report/static/js/2.b3f66551.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/field-report/static/js/2.b3f66551.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cb18f316a2367062ef3",
+    "url": "/field-report/static/js/main.b33aea12.chunk.js"
+  },
+  {
+    "revision": "3c84f0c4ca481ba7567b",
+    "url": "/field-report/static/js/runtime-main.b29fde29.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/field-report/static/media/logo.5d5d9eef.svg"
+  }
+]);
